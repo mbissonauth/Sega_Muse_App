441a58ca905e0a2dc51407f8d1f08c83cffb1dd5
Mokshada <mokshada2025@gmail.com>
2026-02-23 23:37:05 +0400

    changes

--- src/createClient.js
+++ src/createClient.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase =createClient(
-    "https://khnyundsxwrnmxmmdtlt.supabase.co", 
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtobnl1bmRzeHdybm14bW1kdGx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1Mjg4MDgsImV4cCI6MjA4NDEwNDgwOH0.eDuDrNIy7668x15G_kWH_U5-UNy3BRFGJRd0cINhs8Y"
+ import.meta.env.REACT_APP_SUPABASE_URL,
+ import.meta.env.REACT_APP_SUPABASE_ANON_KEY
 );
